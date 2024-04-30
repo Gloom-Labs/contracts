92a5cfc20d9035134d955bc523ab7639424007da
Markeljan Sokoli <Markeljan19@gmail.com>
2024-04-30 15:42:48 -0400

    presale wip

--- src/Gloomers.sol
+++ src/Gloomers.sol
@@ -1,21 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {ERC721A} from "erc721a/contracts/ERC721A.sol";
+import {IERC721A, ERC721A} from "erc721a/contracts/ERC721A.sol";
+import {ERC721AQueryable} from "erc721a/contracts/extensions/ERC721AQueryable.sol";
+import {StartTokenIdHelper} from "erc721a/contracts/extensions/StartTokenIdHelper.sol";
 import {WhitelistVerifier} from "../src/WhitelistVerifier.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
 
-contract Gloomers is ERC721A, Ownable, WhitelistVerifier, ERC721Royalty {
+contract Gloomers is ERC721A, WhitelistVerifier, Ownable, ERC2981, ERC721AQueryable, StartTokenIdHelper {
     uint256 public constant MAX_SUPPLY = 3333;
     uint256 public constant PRICE_PER_TOKEN = 0.03 ether;
     uint256 public constant MAX_MINT_PER_WALLET = 3;
-    uint256 public constant START_TOKEN_ID = 3334;
     bytes32 public constant PROVENANCE_HASH = 0x5158cf3ac201d8d9dfe63ac7c7d1e7aa58b7c33426665c9bf643e0003e095e2f;
     uint256 public constant MINT_START_TIMESTAMP = 1714838400; // Sat May 04 2024 16:00:00 GMT
     uint256 public constant WHITELIST_MINT_DURATION = 3 hours;
 
     bool public revealed = false;
+
     MintState public mintState = MintState.DISABLED;
 
     enum MintState {
@@ -35,10 +37,10 @@ contract Gloomers is ERC721A, Ownable, WhitelistVerifier, ERC721Royalty {
 
     error MintingDisabled();
     error PublicSaleNotActive();
-    error WhitelistError();
     error ExceedsMaxMintPerWallet();
     error ExceedsMaxSupply();
     error InsufficientFunds();
+    error NonEOACaller();
 
     modifier publicSaleActive() {
         if (mintState != MintState.PUBLIC) {
@@ -68,13 +70,6 @@ contract Gloomers is ERC721A, Ownable, WhitelistVerifier, ERC721Royalty {
         _;
     }
 
-    modifier validatedWhitelistRequest(bytes32 hash, bytes memory signature) {
-        if (!isValidWhitelistRequest(hash, signature)) {
-            revert WhitelistError();
-        }
-        _;
-    }
-
     modifier notOverWalletLimit(uint256 quantity) {
         if (_mintedPerWallet[msg.sender] + quantity > MAX_MINT_PER_WALLET && msg.sender != owner()) {
             revert ExceedsMaxMintPerWallet();
@@ -82,18 +77,31 @@ contract Gloomers is ERC721A, Ownable, WhitelistVerifier, ERC721Royalty {
         _;
     }
 
-    constructor() ERC721A("Gloomers", "GLOOMERS") Ownable(msg.sender) {
+    modifier callerIsEOA() {
+        if (tx.origin != msg.sender) {
+            revert NonEOACaller();
+        }
+        _;
+    }
+
+    constructor(address whitelistSigner_, uint256 startTokenId_)
+        ERC721A("Gloomers", "GLOOMERS")
+        Ownable(msg.sender)
+        WhitelistVerifier(whitelistSigner_)
+        StartTokenIdHelper(startTokenId_)
+    {
         _setDefaultRoyalty(msg.sender, 500);
     }
 
     function mint(uint256 quantity)
         external
         payable
-        publicSaleActive
         notDisabled
+        callerIsEOA
         mintsAvailable(quantity)
         fundsAttached(quantity)
         notOverWalletLimit(quantity)
+        publicSaleActive
     {
         _mintedPerWallet[msg.sender] += quantity;
         _mint(msg.sender, quantity);
@@ -103,8 +111,9 @@ contract Gloomers is ERC721A, Ownable, WhitelistVerifier, ERC721Royalty {
 
     function whitelistMint(uint256 quantity, bytes32 hash, bytes memory signature)
         external
-        publicSaleActive
+        payable
         notDisabled
+        callerIsEOA
         mintsAvailable(quantity)
         fundsAttached(quantity)
         notOverWalletLimit(quantity)
@@ -116,7 +125,33 @@ contract Gloomers is ERC721A, Ownable, WhitelistVerifier, ERC721Royalty {
         emit WhitelistClaimed(msg.sender, quantity);
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    function presaleMint(uint256 quantity, bytes32 hash, bytes memory signature)
+        external
+        payable
+        notDisabled
+        callerIsEOA
+        mintsAvailable(quantity)
+        fundsAttached(quantity)
+        notOverWalletLimit(quantity)
+        validatedWhitelistRequest(hash, signature)
+    {
+        _presaleMintedPerWallet[msg.sender] += quantity;
+        emit PresaleClaimed(msg.sender, quantity);
+    }
+
+    function _startTokenId() internal view override returns (uint256) {
+        return startTokenId();
+    }
+
+    function _sequentialUpTo() internal view override returns (uint256) {
+        return _startTokenId() + MAX_SUPPLY;
+    }
+
+    function _baseURI() internal view override(ERC721A) returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override(IERC721A, ERC721A) returns (string memory) {
         if (!_exists(tokenId)) {
             revert URIQueryForNonexistentToken();
         }
@@ -126,43 +161,28 @@ contract Gloomers is ERC721A, Ownable, WhitelistVerifier, ERC721Royalty {
         return bytes(baseURI).length != 0 ? result : "";
     }
 
-    function triggerMintState() external {
-        uint256 timestamp = block.timestamp;
-        if (timestamp <= MINT_START_TIMESTAMP) {
-            revert MintingDisabled();
-        }
-        if (timestamp < MINT_START_TIMESTAMP + WHITELIST_MINT_DURATION) {
-            mintState = MintState.WHITELIST;
-        } else {
-            mintState = MintState.PUBLIC;
-        }
-        emit MintStateUpdated(mintState_);
-    }
-
     function setTokenUri(string calldata baseTokenURI_) external onlyOwner {
         revealed = true;
         _baseTokenURI = baseTokenURI_;
-        emit BatchMetadataUpdate(START_TOKEN_ID, (type(uint256).max));
+        emit BatchMetadataUpdate(_startTokenId(), (type(uint256).max));
     }
 
-    function setDefaultRoyalty(address receiver, uinit96 feeNumerator) external onlyOwner {
+    function setDefaultRoyalty(address receiver, uint96 feeNumerator) public onlyOwner {
         _setDefaultRoyalty(receiver, feeNumerator);
     }
 
-    function _startTokenId() internal pure override returns (uint256) {
-        return START_TOKEN_ID;
-    }
-
-    function _sequentialUpTo() internal view override returns (uint256) {
-        return START_TOKEN_ID + MAX_SUPPLY;
-    }
-
-    function _baseURI() internal view override returns (string memory) {
-        return _baseTokenURI;
+    function updateWhitelistSigner(address whitelistSigner_) public onlyOwner {
+        _updateWhitelistSigner(whitelistSigner_);
     }
 
     // Required overrides
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A, ERC2981) returns (bool) {
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(IERC721A, ERC721A, ERC2981)
+        returns (bool)
+    {
         // Supports the following `interfaceId`s:
         // - IERC165: 0x01ffc9a7
         // - IERC721: 0x80ac58cd
--- src/WhitelistVerifier.sol
+++ src/WhitelistVerifier.sol
@@ -6,13 +6,26 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 contract WhitelistVerifier {
     address public whitelistSigner;
 
+    error InvalidWhitelistRequest();
+
     constructor(address _whitelistSigner) {
         whitelistSigner = _whitelistSigner;
     }
 
+    modifier validatedWhitelistRequest(bytes32 hash, bytes memory signature) {
+        if (!isValidWhitelistRequest(hash, signature)) {
+            revert InvalidWhitelistRequest();
+        }
+        _;
+    }
+
     // Validate signer appproved the hash (address + quantity) for whitelist minting
-    function isValidWhitelistRequest(bytes32 hash, bytes memory signature) public view returns (bool) {
+    function isValidWhitelistRequest(bytes32 hash, bytes memory signature) private view returns (bool) {
         address signer = ECDSA.recover(hash, signature);
         return signer == whitelistSigner;
+    }
+
+    function updateWhitelistSigner(address _whitelistSigner) internal {
+        whitelistSigner = _whitelistSigner;
     }
 }
